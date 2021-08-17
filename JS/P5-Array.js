1e92cc0b5f79580f438a77f548b1a3d9af93e686
DARSHAN S KAMBLE <darshankamble7371@gmail.com>
2021-08-17 15:51:51 +0530

    Practice Set 3

--- JS/P5-Array.js
+++ JS/P5-Array.js
@@ -8,49 +8,49 @@ let num = [12, 25, 33.33, 50, 258, 45, 13, 2, 9];
 let groceries = ["Egg", "Potato", "Tomato", "Banana", "Apple"];
 
 // Iterating an array-------------------------------
-// for (let i = 0; i < students.length; i++) {
-//     // console.log(students[i]);
-// }
-// for (let names in students) {
-//     // console.log(students[names]);
-// }
-// for (let names of students) {
-//     console.log(names);
-// }
-// students.forEach((e) => {
-//     // console.log(e)
-// })
+for (let i = 0; i < students.length; i++) {
+    console.log(students[i]);
+}
+for (let names in students) {
+    console.log(students[names]);
+}
+for (let names of students) {
+    console.log(names);
+}
+students.forEach((e) => {
+    console.log(e)
+})
 
 // ARRAY METHODS------------------------------
 let std = ["Drashan", "Pratik", 12, 0, "Om", "Ram", "Sham"];
 
 // which change main array
-// console.log(std.pop());
-// console.log(std
-// console.log(std.push("Yash"));
-// console.log(std
-// console.log(std.shift())
-// console.log(std
-// console.log(std.unshift("Jai"));
-// console.log(std
-// console.log(delete std[0]);
-// console.log(std
-// console.log(std.splice(6, 1));
+console.log(std.pop());
+console.log(std);
+console.log(std.push("Yash"));
+console.log(std);
+console.log(std.shift())
+console.log(std);
+console.log(std.unshift("Jai"));
+console.log(std);
+console.log(delete std[0]);
+console.log(std);
+console.log(std.splice(6, 1));
 
 // which dont change main array , it creats new arry
-// console.log(std.concat(groceries));
-// console.log(std
-// console.log(std.slice(0, 2)) 
-// console.log(std
+console.log(std.concat(groceries));
+console.log(std);
+console.log(std.slice(0, 2))
+console.log(std);
 
-// console.log(std.indexOf("Ram"));
-// console.log(std.lastIndexOf("Sham"));
-// console.log(std.includes("Yash"));
+console.log(std.indexOf("Ram"));
+console.log(std.lastIndexOf("Sham"));
+console.log(std.includes("Yash"));
 
 // Sort methods  ,  it creats new arry
 console.log(std.sort());
 console.log(std);
-// console.log(std.reverse())
+console.log(std.reverse())
 
 num = [12, 25, 33.33, 50, 258, 45, 13, 2, 9];
 
@@ -60,21 +60,21 @@ console.log(obj);
 let mapj = num.map((e, i) => e * 2)
 console.log(mapj) //[24, 50, 66.66, 100, 516, 90, 26, 4]
 
-// let filterj = num.filter((e, i) => {
-//     if (e % 2 != 0) {
-//         return e;
-//     }
-// })
-// console.log(filterj) // returns all even num in array
+let filterj = num.filter((e, i) => {
+    if (e % 2 != 0) {
+        return e;
+    }
+})
+console.log(filterj) // returns all even num in array
 
-// let reducej = num.reduce((e, i) => {
-//     console.log(e, i)
-//     return (e + i)
-// })
-// console.log(reducej)
+let reducej = num.reduce((e, i) => {
+    console.log(e, i)
+    return (e + i)
+})
+console.log(reducej)
 
-// let everyj = num.every((e) => e < 340); // it check the condition for every ele and return true or false 
-// console.log(everyj);
+let everyj = num.every((e) => e < 340); // it check the condition for every ele and return true or false 
+console.log(everyj);
 
-// let findj = num.find((e) => e < 10);
-// console.log(findj); // 2
+let findj = num.find((e) => e < 10);
+console.log(findj); // 2
---
+++ JS/P6-Object.js
@@ -0,0 +1,60 @@
+console.log("It Works");
+
+// OBJECTS
+
+// EXAMPLES--------------------------------------------------------
+let stdInfo = {
+    name: "Om",
+    greeting: "Good marning",
+    hobby: "Cricket",
+    favLang: "JavaScript",
+    aim: "Web Dev"
+}
+
+let marks = {
+    ram: 33,
+    sham: 38,
+    darshan: 40,
+    om: 20,
+    pratik: 25
+}
+
+// CONSTRUCTOR IN OBJ
+
+function car(name, speed) {
+    this.carName = name;
+    this.carSpeed = speed;
+}
+let car1 = new car("BMW", 180);
+console.log(car1);
+
+// A REAL LIFE EXAMPLE ---------------------------------------------
+let user = {
+    firstName: "Darshan",
+    lastName: "Kamble",
+    loginCount: 32,
+    faceBookSignIn: true,
+    courseList: [],
+    buyAnyCourse: ((courseName) => {
+        user.courseList.push(courseName);
+    }),
+    courseCount: (() => {
+        return `${user.firstName} is enrolled in total ${user.courseList.length} courses`
+    })
+}
+
+user.buyAnyCourse("Python Course");
+user.buyAnyCourse("JavaScript Course");
+// console.log(user.courseCount())
+// END-------------------------------------------------------------------
+
+// ITERATING OBJECT 
+
+let val1 = Object.keys(marks);
+// console.log(val1) // only iterate key and convert them into an array
+
+for (let i = 0; i < val1.length; i++) {
+    const ele = marks[val1[i]];
+    // console.log(ele);
+    // console.log(i);
+}
