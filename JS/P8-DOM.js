0b4ee79714aaec4367e4a604cce6623e599ebf46
DARSHAN S KAMBLE <darshankamble7371@gmail.com>
2021-08-25 22:35:37 +0530

    Practice set 5

--- JS/P8-DOM.js
+++ JS/P8-DOM.js
@@ -1,31 +1,34 @@
 console.log("Its working");
 
 // WINDOWS OBJECT
-// console.log(window)
-// console.log(window.prompt());
-// console.log(window.confirm());
-// console.log(window.close())
-// console.log(window.find("Darshan"));
-// console.log(window.focus())
-// console.log(window.localStorage);
-// console.log(window.location);
-// console.log(window.history.go(-1));
+console.log(window)
+console.log(window.prompt());
+console.log(window.confirm());
+console.log(window.close())
+console.log(window.find("Darshan"));
+console.log(window.focus())
+console.log(window.localStorage);
+console.log(window.location);
+console.log(window.history.go(-1));
 
 
 // DOM 
 
-// let containerj = document.getElementsByClassName("container");
-// console.log(containerj);
-// console.log(containerj[0].innerHTML = "This is container");
-// let boxj = document.getElementById("box")
-//     // boxj.innerHTML = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+let containerj = document.getElementsByClassName("container");
+console.log(containerj);
+console.log(containerj[0].innerHTML = "This is container");
+let boxj = document.getElementById("box")
+boxj.innerHTML = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
 
-// Array.from(containerj).forEach((e) => {
-//     e.style.background = "red";
-// });
+Array.from(containerj).forEach((e) => {
+    e.style.background = "red";
+});
 
 let cond = document.querySelectorAll("#box")
-    // console.log(cond.length)
-    // Array.from(cond).forEach((e) => {
-    //     e.style.background = "red"
-    // })
+console.log(cond.length)
+Array.from(cond).forEach((e) => {
+    e.style.background = "red"
+})
+
+
+console.log(document, )
